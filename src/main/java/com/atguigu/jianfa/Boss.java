6b5d3ead0ceb588269d51a51f3d2f9d705619400
“hushicheng” <949579890@qq.com>
2020-08-20 16:40:41 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/jianfa/Boss.java
@@ -0,0 +1,10 @@
+package com.atguigu.jianfa;
+
+public class Boss {
+    public static void main(String[] args) {
+
+        System.out.println("我是老板");
+        System.out.println("我是老板");
+        System.out.println("我是老板");
+    }
+}
